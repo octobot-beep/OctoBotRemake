4055c4b71419a93fae0cf52ac722bfd2480e20f5
octobot-beep <octobotai@gmail.com>
2025-02-22 21:16:32 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,16 +4,16 @@ module.exports = {
     credits: "rejardgwapo",
     cooldown: 16,		
     execute(api, event, args, commands) {
-        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚊𝚝 https://github.com/hardasf/OctoBotRemake . 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
-        helpMessage += '💮═══════════════💮\n';
+        let helpMessage = '𝙷𝚒!: . 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
+        helpMessage += '🍜═══════════════🍜\n';
         commands.forEach((command, name) => {
             helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
             helpMessage += `𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description}\n`;
             helpMessage += `𝚁ole: ${command.role} | CD: ${command.cooldown}\n`;
             helpMessage += `Credits: ${command.credits}\n`;
-        helpMessage += '💮═══════════════💮\n';
+        helpMessage += '🍜═══════════════🍜\n';
         });
-        helpMessage += '💬https://facebook.com/leechshares';
+        helpMessage += '💬Owner is Ram Bermudez';
         api.sendMessage(helpMessage, event.threadID);
     }
 };
